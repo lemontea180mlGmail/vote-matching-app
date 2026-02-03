3f1ad40bc5d2b30efe0d91b4acfdcb54888369b5
lemontea180mlGmail <lemontea180ml@gmail.com>
2026-02-03 19:36:00 +0900

    v0.9.2へ修正

--- src/App.jsx
+++ src/App.jsx
@@ -207,7 +207,7 @@ function App() {
                         >
                             スタート
                         </button>
-                        <p className="mt-4 text-xs text-gray-400">v0.9.1</p>
+                        <p className="mt-4 text-xs text-gray-400">v0.9.2</p>
                     </div>
                 )}
 
